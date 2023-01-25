9755737ade64353ee44510d6eee8d044eb9135ec
Frances O'Brien <jfrancesobrien@gmail.com>
2023-01-25 11:29:56 -0500

    delete and main buttons work

--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -12,8 +12,8 @@ export default function EditTask(){
         .then(res => setData(res.data))
     }, [])
 
-    const editTheTask = e => {
-        e.preventDefault()
+    const editTheTask = (e) => {
+        e.preventDefault();
         const updatedTask = {
             task: e.target.task.value,
             category: e.target.category.value,
@@ -21,14 +21,17 @@ export default function EditTask(){
             points: e.target.points.value,
             isComplete: false,
         }
+
+
         editTask(id, updatedTask).then( () => {
             nav(`/${id}`)
         })
+        
     }
 
     return(
         <div>
-            <form onSubmit={editTheTask}>
+            <form onSubmit={editTheTask()}>
                 task:<input type='text' name='task' defaultValue={data.task}/>
                 categroy:<input type='text' name='category' defaultValue={data.category}/>
                 details:<input type='text' name='details' defaultValue={data.details}/>
